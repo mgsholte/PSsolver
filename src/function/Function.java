779e55b27fec2bf07cb217c64ac3a984ed4aeb2c
mgsholte (Laptop) <mgsholte@physics.utexas.edu>
2013-10-24 17:41:41 -0500

    Initial migration of existing files to git

---
+++ src/function/Function.java
@@ -0,0 +1,117 @@
+package function;
+
+/**
+ * The length of the domain (size) of the function needs to be an integer
+ * multiple of the stepSize.
+ * 
+ * @author Matthew Butcher
+ * @version Version 1, 10/14/13
+ */
+public class Function {
+
+	private final double[] vals;
+	private final Domain domain;
+
+	/**
+	 *  Create a scalar function of 1 variable from a pre-filled array of values using a 
+	 *  constant spacing between values of the independent variable.
+	 *  
+	 * @param function - the array of function values
+	 * @param lb - the smallest value of the indep var (i.e., at function[0])
+	 * @param ub - the largest value of the indep var (i.e., at function[length-1])
+	 * @param stepSize - the uniform spacing between indep var values
+	 */
+	public Function(double[] fvals, double lb, double ub, double stepSize) {
+		vals = fvals;
+		domain = new Domain(lb, ub, stepSize);
+	}
+	
+	public Function(double[] fvals, Domain d) {
+		vals = fvals;
+		domain = d;
+	}
+	
+	/// getter methods
+	public double getLB() { return domain.lb; }
+	public double getUB() { return domain.ub; }
+	public double getH()  { return domain.h; }
+	public Domain getDomain() { return domain; }
+
+	// /**standard string conversion
+	// *
+	// */
+	// public String toString(){
+	// return "stuff";//not done here yet
+	// }
+	
+	public double evalAt(double x) {
+		if (!domain.contains(x)) 
+			throw new NotInDomainException(x);
+		int idx = (int) Math.round((x-domain.lb)/domain.h);
+		return vals[idx];
+	}
+		
+	/**
+	 * adds two function objects
+	 * 
+	 */
+	public Function plus(Function f) {
+		if (!domain.equals(f.domain))
+			throw new DomainMismatchException();
+		int n = vals.length;
+		double[] sum = new double[n];
+		for (int i = 0; i < n; ++i) {
+			sum[i] = this.vals[i] + f.vals[i];
+		}
+		return new Function(sum, domain);
+	}
+
+	/**
+	 * multiplies two function objects
+	 * 
+	 */
+	public Function times(Function f) {
+		if (!domain.equals(f.domain))
+			throw new DomainMismatchException();
+		int n = vals.length;
+		double[] product = new double[n];
+		for (int i = 0; i < n; ++i) {
+			product[i] = this.vals[i] * f.vals[i];
+		}
+		return new Function(product, domain);
+	}
+
+	/** Perform a definite integral using the given bounds */
+	public double integrate(double lowBd, double upBd) {
+		Domain iDom = new Domain(lowBd, upBd, domain.h);
+		if (!iDom.isSubsetOf(domain))
+			throw new FunctionException(
+					"Tried to integrate a function on the interval "+iDom+", "
+					+"but it is only defined on "+domain
+					);
+		/// NB: endIdx is 1 past the last value that should be included in the integral
+		int startIdx = (int) Math.floor((lowBd-domain.lb)/domain.h), 
+			endIdx = (int) Math.ceil((upBd-domain.ub)/domain.h) + vals.length;
+		
+		double sum = 0;
+		for (int i = startIdx; i < endIdx; ++i) {
+			sum += vals[i]; // factored out 'h' since it is constant
+		}
+		return domain.h*sum;
+	}
+	
+	/** Convenience function to integrate over the entire domain */
+	public double integrate() {
+		return integrate(getLB(), getUB());
+	}
+
+	/**
+	 * This requires f to have the same size and step size as our fOfx so it
+	 * mostly is built specifically for computing the norm of a wavefunction
+	 */
+	public double innerProduct(Function f) {
+		if (!domain.equals(f.domain))
+			throw new DomainMismatchException();
+		return this.times(f).integrate();
+	}
+}
---
+++ src/utils/CalculatePsi.java
@@ -0,0 +1,81 @@
+package utils;
+
+import java.awt.Graphics;
+
+/**
+ * This class will (eventually) take an input of a potential function and some situational parameters and calculate 
+ * energies along with their associated wavefunctions
+ * 
+ * @author Matthew Butcher 
+ * @version Version 1.0, Started 10-2-13
+ */
+public class CalculatePsi
+{   //all of the constants named here are arbitrary, and can be changed later to match the actual problem
+    public static final double H = .01;
+    public static final double H2 = H*H;
+    public static final double DEL_E = .01;
+    public static final double V_MIN = -50;
+    public static final double WIDTH = 1;
+    public static final int INDEX = (int) (WIDTH/H);
+    public static final double EPSILON = .001;
+    /**Use the shooting method to guess the energy, and numerov method to integrate psi.
+     * guess a minimum E (E1) and increment it by DEL_E to get second guess E2.  Integrate 
+     * psi with E1 and E2 and check if psie1[end]*psie2[end] < 0. If not, increment
+     * E2 again and repeat until the two guesses give opposite signed boundaries (we want psi = 0
+     * at the boundaries).  Then find E3 = (E1 + E2)/2 and repeat until Eavg gives boundary within
+     * a good error bound.  This will give a psi with associated eigenvalue.
+     */
+    
+    public static void main(){
+        double[] testPsi1 = new double[INDEX];
+        double[] testPsi2 = new double[INDEX];
+        double[] testPsi3 = new double[INDEX];
+        testPsi1[0] = 0;
+        testPsi1[1] = .01;
+        testPsi2[0] = 0;
+        testPsi2[1] = .01;
+        testPsi3[0] = 0;
+        testPsi3[1] = .01;
+        double E1 = 0;
+        double E2 = E1 + DEL_E;
+        double E3 = 0;
+        
+        while (Math.abs(E1 - E2) > EPSILON){//test if energy is guessed within a specified error bound
+            for (int i = 1; i < INDEX - 1; i++){//calculate psi for two energy guesses
+                testPsi1[i + 1] =  ((2.0 - 10*H2*(-(E1 + V_MIN))/12)*testPsi1[i] - (1 + H2*(-(E1 + V_MIN))/12)*testPsi1[i - 1])/(1 + H2*(-(E1 + V_MIN))/12);
+                testPsi2[i + 1] =  ((2.0 - 10*H2*(-(E2 + V_MIN))/12)*testPsi2[i] - (1 + H2*(-(E2 + V_MIN))/12)*testPsi2[i - 1])/(1 + H2*(-(E2 + V_MIN))/12);
+            }
+            if (testPsi1[INDEX - 1]*testPsi2[INDEX - 1] > 0)
+                E2 += DEL_E;//increment energy guess by 1 if psi hasn't crossed the x axis at the boundary(will be updated to be inf)
+            else{
+                E3 = (E1 + E2)/2;//get a more accurate energy eigenvalue
+                for (int i = 1; i < INDEX - 1; i++){
+                    testPsi3[i + 1] =  ((2.0 - 10*H2*(-(E3 + V_MIN))/12)*testPsi3[i] - (1 + H2*(-(E3 + V_MIN))/12)*testPsi3[i - 1])/(1 + H2*(-(E3 + V_MIN))/12);
+                }
+                if (testPsi3[INDEX - 1]*testPsi2[INDEX - 1] < 0)//picks new energy guesses based on end behavior
+                    E1 = E3;
+                else
+                    E2 = E3;
+            }
+        }
+        
+        
+        //these are just some outputs I was using to check if the behavior was what I expected
+        System.out.println(E1 + " " + E2 + " " + E3);
+        for (int i = 0; i < INDEX; i++){
+            System.out.print(testPsi1[i] + " ");
+        }
+        
+        DrawingPanel testGraph = new DrawingPanel((int)(200*WIDTH), 200);
+        Graphics graphPsi = testGraph.getGraphics();
+        graphPsi.drawLine(0,100,(int)(200*WIDTH),100);
+        graphPsi.drawLine((int)(100*WIDTH),0,(int)(100*WIDTH),200);
+        for (int i = 0; i < INDEX; i++){
+            graphPsi.fillOval((int) (i*200*WIDTH/INDEX), (int) (100 - testPsi1[i]*100), 2, 2);
+        }
+    }
+
+    
+    
+    
+}
